61ef4578e138a5f19dedc15195a3af1b75acaea9
Ryo ikegata <ryo.ike.6153@gmail.com>
2023-10-18 17:41:38 +0900

    Add:追加機能を作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,136 @@
+import { useState } from 'react';
 import './App.css';
-import React from 'react';
-import Game from './Components/Game';
 
-
-
-function App() {
+function Square({ value, index,  onSquareClick }) {
   return (
-    <div>
-      <Game  />
+    <button className="square" id={index} onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  
+  if (winner) {
+    status = 'Winner: ' + winner;
+    console.log(winner);
+  }else if(squares.every((square) => square !== null)){
+    status = 'Draw';
+  }else{
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+
+  //Squaresを繰り返し
+  const board = [];
+  for (let row = 0; row < 3; row++) {
+    const squareInRow = [];
+    for (let col = 0; col < 3; col++) {
+      const squareIndex = row * 3 + col;
+      squareInRow.push(
+        <Square
+          key={squareIndex}
+          index={squareIndex}
+          className={squareIndex}
+          value={squares[squareIndex]}
+          onSquareClick={() => handleClick(squareIndex)}
+        />
+      );
+    }
+    board.push(<div key={row} className="board-row">{squareInRow}</div>);
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+    {board}
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      
+      const winnerLine = lines[i];
+      winnerLine.forEach(index => {
+      const square = document.getElementById(index);
+      if (square) {
+        square.style.backgroundColor = 'red';
+      }
+    });
+    return squares[a];
+  }
+}
+return null;
+}
+
---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+
+
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
